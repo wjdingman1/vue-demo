9d83c06267d5f97007b8c1d55a3a0a6d9c47214c
wdingman <wjdingman1@gmail.com>
2021-03-30 16:12:31 -0400

    Add some clarity comments

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
-import routes from "./routes/routes";
 import Router from "vue-router";
+import routes from "./routes/routes";
 import ToggleButton from "vue-js-toggle-button";
 
 Vue.config.productionTip = false;
 
+/* Tell Vue to register our routes */
 Vue.use(Router);
+/* Tell Vue to register our toggle component */
 Vue.use(ToggleButton);
 
 const router = new Router({ routes });
 
+/* Mound the Vue instance in index.html */
 new Vue({
   router,
   render: h => h(App)
