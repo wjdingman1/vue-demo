b04d5497ec68da56430a9c73c5612c45e49c00d7
wdingman <wjdingman1@gmail.com>
2021-04-15 10:41:09 -0400

    Add vuex skeleton

--- src/main.js
+++ src/main.js
@@ -2,19 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import Router from "vue-router";
 import routes from "./routes/routes";
+import store from "./state/store";
 import ToggleButton from "vue-js-toggle-button";
 
 Vue.config.productionTip = false;
 
-/* Tell Vue to register our routes */
+/* Tell Vue to use vue-router */
 Vue.use(Router);
 /* Tell Vue to register our toggle component */
 Vue.use(ToggleButton);
 
+/* Create the router instance with our defined routes */
 const router = new Router({ routes });
 
 /* Mound the Vue instance in index.html */
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/state/store.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import main from "./modules/main";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    main
+  }
+});
+
+export default store;
